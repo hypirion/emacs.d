484e617b0e80b7f0386e04a7b69764c456729670
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-25 17:22:36 +0100

    Autoinsert '}'-brackets and newline, add hungry mode.

--- hypirion-c.el
+++ hypirion-c.el
@@ -1,4 +1,20 @@
 (require 'cc-mode)
+
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+(c-toggle-electric-state 1)
+(c-toggle-hungry-state 1)
+
+(defun hypirion-c-mode-insert-brackets ()
+  (interactive)
+  (insert "{")
+  (let ((pps (syntax-ppss)))
+    (when (and (eolp) (not (or (nth 3 pps) (nth 4 pps))))
+      (c-indent-line)
+      (insert "\n\n}")
+      (c-indent-line)
+      (forward-line -1)
+      (c-indent-line))))
+
+(define-key c-mode-base-map "{" 'hypirion-c-mode-insert-brackets)
 
 (provide 'hypirion-c)
