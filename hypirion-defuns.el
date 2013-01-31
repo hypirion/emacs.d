6fc8c07ba41d6da1daaa3cd1f1ac3a7a8ca2286b
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-31 19:15:16 +0100

    Remove arrow keybindings. Real men should live without.

--- hypirion-defuns.el
+++ hypirion-defuns.el
@@ -4,4 +4,9 @@
            (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                      "λ" 'decompose-region)))))))
 
+(dolist (kbd-str '("<left>" "<right>" "<up>" "<down>" "<C-left>" "<C-right>"
+                   "<C-up>" "<C-down>" "<M-left>" "<M-right>" "<M-up>"
+                   "<M-down>"))
+  (eval `(global-unset-key (kbd ,kbd-str))))
+
 (provide 'hypirion-defuns)
--- hypirion-parens.el
+++ hypirion-parens.el
@@ -1,6 +1,16 @@
 ;; paredit
 (require 'paredit)
 
+;; setup splice-sexp-killing-forward
+(define-key paredit-mode-map (kbd "M-n") 'paredit-splice-sexp-killing-forward)
+(define-key paredit-mode-map (kbd "M-p") 'paredit-splice-sexp-killing-backward)
+
+;; We're professionals and won't allow arrow keys
+(define-key paredit-mode-map (kbd "C-<right>") nil)
+(define-key paredit-mode-map (kbd "C-<left>") nil)
+(define-key paredit-mode-map (kbd "M-<down>") nil)
+(define-key paredit-mode-map (kbd "M-<up>") nil)
+
 ;; Highlight Parens
 (require 'highlight-parentheses)
 (hl-paren-set 'hl-paren-colors
