9e425bc0372dc29e65f6c6a3d5469a93c23fad2f
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-25 00:34:27 +0200

    Make <RET> in erlang-mode newline-and-indent.

--- hypirion-erlang.el
+++ hypirion-erlang.el
@@ -2,4 +2,27 @@
 
 (add-to-list 'auto-mode-alist '("\\.erl\\'" . erlang-mode))
 
+;; Fix erlang's enter key
+(add-hook 'erlang-mode-hook
+          (lambda ()
+            (define-key erlang-mode-map (kbd "RET") 'newline-and-indent)))
+
 (provide 'hypirion-erlang)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
