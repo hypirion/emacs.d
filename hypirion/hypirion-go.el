6e35c0d8187a2dd058d79adf39c833bafed7e03a
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-01-04 19:59:22 +0100

    Let go-autocomplete handle autocompletion setup.

--- hypirion/hypirion-go.el
+++ hypirion/hypirion-go.el
@@ -2,6 +2,5 @@
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-(add-to-list 'ac-modes 'go-mode) ;; Seems broken somehow.
 
 (provide 'hypirion-go)
