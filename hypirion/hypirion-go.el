a364a5949d59d5a96a38ab3119c105e379e07d81
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-08-10 11:59:31 +0200

    gofmt on save

--- hypirion/hypirion-go.el
+++ hypirion/hypirion-go.el
@@ -1,6 +1,10 @@
 ;; Well, that wasn't hard.
 (require 'go-autocomplete)
 (require 'auto-complete-config)
+(require 'go-mode)
+
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+(add-hook 'before-save-hook 'gofmt-before-save)
+
 
 (provide 'hypirion-go)
