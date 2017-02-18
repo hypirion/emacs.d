d323877c7fcdc39f83f28151b5dd20c56efe481d
Jean Niklas L'orange <jeannikl@hypirion.com>
2017-02-18 21:20:06 +0100

    godef-jump

--- hypirion/hypirion-go.el
+++ hypirion/hypirion-go.el
@@ -7,7 +7,14 @@
 
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
+(defun hypirion-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark))
+
+(add-hook 'go-mode-hook 'hypirion-go-mode-hook)
 
 
 (provide 'hypirion-go)
