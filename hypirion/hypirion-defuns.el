cae407a0ca5f3f23c2a68c58be871f7bebc61675
Jean Niklas L'orange <jeannikl@hypirion.com>
2021-10-05 13:28:32 +0200

    My left pinky is killing me 🎶

--- hypirion/hypirion-defuns.el
+++ hypirion/hypirion-defuns.el
@@ -6,10 +6,17 @@
 
 (dolist (kbd-str '("<left>" "<right>" "<up>" "<down>" "<C-left>" "<C-right>"
                    "<C-up>" "<C-down>" "<M-left>" "<M-right>" "<M-up>"
-                   "<M-down>"))
+                   "<M-down>" "C-x C-s" "C-x C-f" "C-x C-v" "C-g" "C-x o" "C-x b"))
   (eval `(global-unset-key (kbd ,kbd-str))))
 
 (global-set-key (kbd "<up>") (lambda () (interactive) (scroll-down 1)))
 (global-set-key (kbd "<down>") (lambda () (interactive) (scroll-up 1)))
+
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-f") 'find-file)
+(global-set-key (kbd "s-v") 'find-alternate-file)
+(global-set-key (kbd "s-g") 'keyboard-escape-quit)
+(global-set-key (kbd "s-o s-o") 'other-window)
+(global-set-key (kbd "s-b") 'ivy-switch-buffer) ;; really M-s-b
 
 (provide 'hypirion-defuns)
